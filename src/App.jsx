161c5e4cdd5718f0dde5492ccb45f63851c7b1ea
te182d <93023422+suhrobh1@users.noreply.github.com>
2021-12-22 17:17:44 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+
+function App() {
+  
+  const [pokemon, setPokemon] = useState([]);
+  
+
+useEffect(() => {
+fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+  .then((res) => {
+    console.log(res);
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res);
+    setPokemon(res.results);
+  })
+  .catch((err) => console.log(err))
+}, [])
+
+  return (
+    <div className="App">
+      {
+        pokemon.map((character, index) => {
+          return (
+          <p key={index}>{index + 1} {character.name}</p>
+          )
+        })
+      }
+       
+    </div>
+  );
+}
+
+export default App;
